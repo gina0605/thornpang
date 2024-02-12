103ebb543ea9bcd390c0ce1d1ea84086e62ba17a
gina0605 <jina0605@gmail.com>
2024-02-13 02:00:04 +0900

    Update SerachBar

--- src/components/lyrics/searchBar.tsx
+++ src/components/lyrics/searchBar.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export interface SearchBarProps {
   param: string | null;
@@ -13,53 +13,47 @@ export const SearchBar = ({ param }: SearchBarProps) => {
   const [activated, setActivated] = useState(param !== null);
   const router = useRouter();
 
+  useEffect(() => {
+    setActivated(true);
+  }, []);
+
   return (
     <div className="w-full h-9 flex">
-      <div className="w-0 grow bg-gradient-to-r from-slate-500/30 to-white" />
-      <div className="w-full h-9 max-w-screen-md flex items-center justify-start pl-2 pr-6 relative">
-        <div
-          className={`h-8 flex items-center rounded transition-[width] duration-300 ${
-            activated ? "border-slate-500 border w-full px-2" : "w-8"
-          }`}
-        >
-          {activated && (
-            <input
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              onKeyUp={(e) => {
-                if (e.key === "Enter") router.push(`/lyrics?keyword=${text}`);
-              }}
-              className="border-0 grow outline-none"
-            />
-          )}
+      <div
+        className={`w-0 grow bg-gradient-to-r from-slate-500 to-white transition-opacity duration-400 ease-in-out ${
+          activated ? "opacity-30" : "opacity-0"
+        }`}
+      />
+      <div
+        className={`w-full h-9 max-w-screen-md flex items-center justify-start px-2 relative transition-opacity duration-400 ease-in-out ${
+          activated ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <div className="h-8 flex items-center rounded border-slate-500 border w-full px-2">
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") router.push(`/lyrics?keyword=${text}`);
+              else if (e.key === "Escape") e.currentTarget.blur();
+            }}
+            className="border-0 grow outline-none"
+          />
           <Image
             src="/icon/search.svg"
             width={24}
             height={24}
             alt="search button"
-            onClick={
-              activated
-                ? () => router.push(`/lyrics?keyword=${text}`)
-                : () => setActivated(true)
-            }
+            onClick={() => router.push(`/lyrics?keyword=${text}`)}
             className="cursor-pointer"
           />
-          {activated && (
-            <Image
-              src="/icon/x.svg"
-              width={24}
-              height={24}
-              alt="x button"
-              onClick={() => {
-                setActivated(false);
-                setText("");
-              }}
-              className="cursor-pointer absolute right-0"
-            />
-          )}
         </div>
       </div>
-      <div className="w-0 grow bg-gradient-to-l from-slate-500/40 to-white" />
+      <div
+        className={`w-0 grow bg-gradient-to-l from-slate-500 to-white transition-opacity duration-400 ease-in-out ${
+          activated ? "opacity-40" : "opacity-0"
+        }`}
+      />
     </div>
   );
 };
