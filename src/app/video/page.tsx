bde07836e2ffcbb369746238967e671635b843a7
gina0605 <jina0605@gmail.com>
2024-02-21 03:18:11 +0900

    Update Row css

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -15,7 +15,7 @@ export default ({
       <div className="w-full max-w-3xl px-1.5 h-9 flex items-center">
         <SearchBar defaultText={param ?? ""} />
       </div>
-      <div className="w-full max-w-3xl flex flex-col md:px-1.5">
+      <div className="w-full max-w-5xl flex flex-col md:px-1.5">
         {data.map((v) => (
           <Row video={v} key={v.link} />
         ))}
--- src/components/video/row.tsx
+++ src/components/video/row.tsx
@@ -13,8 +13,12 @@ export const Row = ({
   const infoText = info.join(" / ");
 
   return (
-    <div className="w-full flex items-start px-1.5 md:px-0 py-1 border-b border-slate-300">
-      <a className="w-32 h-18 relative shrink-0" href={link} target="_blank">
+    <div className="w-full flex items-start px-1.5 md:px-0 py-1 md:py-2 border-b border-slate-300">
+      <a
+        className="w-32 md:w-40 h-18 md:h-[90px] relative shrink-0"
+        href={link}
+        target="_blank"
+      >
         <Image
           src={`/video/${thumbnail}`}
           fill
@@ -23,11 +27,11 @@ export const Row = ({
           className="object-cover"
         />
       </a>
-      <div className="flex flex-col font-pretendard pr-1 pl-2 md:pl-3 pt-0.5 w-[calc(100vw_-_144px)] md:w-[624px] grow">
+      <div className="w-0 grow flex flex-col justify-center font-pretendard pr-1 pl-2 md:pl-3 pt-0.5 md:pt-2">
         <a href={link} target="_blank" className="w-fit">
           <p>{title}</p>
         </a>
-        <a href={link} target="_blank" className="w-fit">
+        <a href={link} target="_blank" className="w-fit md:pb-1.5 md:pt-1">
           <p>{subtitle}</p>
         </a>
         <RowDetail setlist={setlistText} info={infoText} date={date} />
