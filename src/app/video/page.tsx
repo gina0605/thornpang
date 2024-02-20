be46fb185bc05f184b9e9d4e1684bec7f9c2e692
gina0605 <jina0605@gmail.com>
2024-02-21 06:04:00 +0900

    Add search feature for video page

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,7 +1,8 @@
+import { Video } from "@/types";
 import { paramToString } from "@/common/utils";
 import { Row } from "@/components/video/row";
 import { VideoSearchBar } from "@/components/video/videoSearchBar";
-import data from "@/data/video";
+import data, { replaceLogic } from "@/data/video";
 
 export default ({
   searchParams,
@@ -11,13 +12,36 @@ export default ({
   const search = paramToString(searchParams.search);
   const target = paramToString(searchParams.target);
 
+  const keywords = search
+    ? replaceLogic
+        .reduce(
+          (s, { pattern, result }) => s.replaceAll(pattern, result),
+          search.toLowerCase().replaceAll(/[\s.,()\[\]~!?\-\/]+/g, " ")
+        )
+        .split(" ")
+    : [];
+
+  const normalizeStr = (s: string[]) =>
+    s
+      .map((v) => v.replaceAll(/[\s()]+/g, ""))
+      .join(" ")
+      .toLowerCase();
+
+  const match = ({ setlist, info }: Video) => {
+    if (keywords.length === 0) return true;
+    let s: string;
+    if (target === "setlist") s = normalizeStr(setlist);
+    else if (target === "info") s = normalizeStr(info);
+    else s = normalizeStr(setlist) + " " + normalizeStr(info);
+
+    return keywords.every((p) => s.includes(p));
+  };
+
   return (
     <main className="w-full flex flex-col items-center">
       <VideoSearchBar search={search} target={target} />
       <div className="w-full max-w-5xl flex flex-col md:px-1.5">
-        {data.map((v) => (
-          <Row video={v} key={v.link} />
-        ))}
+        {data.map((v) => (match(v) ? <Row video={v} key={v.link} /> : null))}
       </div>
     </main>
   );
--- src/data/video.ts
+++ src/data/video.ts
@@ -1,5 +1,41 @@
 import { Video } from "@/types";
 
+export const replaceLogic = [
+  { pattern: /언플러그드|unplug[^\s]*|acous[^\s]*/g, result: "어쿠" },
+  { pattern: /doc[^\s]*/g, result: "다큐" },
+  { pattern: /오시/g, result: "오렌지의" },
+  { pattern: /빨피/g, result: "빨간피터" },
+  { pattern: /도가미/g, result: "도롱뇽 아가미" },
+  { pattern: /너무/g, result: "너의무리" },
+  { pattern: /매비운/g, result: "매미는비" },
+  { pattern: /시봄|시퍼런\s봄/g, result: "시퍼런봄" },
+  { pattern: /살너밤/g, result: "살아있는" },
+  { pattern: /낯열/g, result: "낯선열대" },
+  { pattern: /물라/g, result: "물가의라" },
+  { pattern: /석맛|석류의\s맛/g, result: "석류의맛" },
+  { pattern: /어달|어려운\s달/g, result: "어려운달" },
+  { pattern: /수하/g, result: "수성의하" },
+  { pattern: /롬네|롬넼/g, result: "로마네스" },
+  { pattern: /위그아/g, result: "위에서그" },
+  { pattern: /넓은?\s?밤/g, result: "넓은밤" },
+  { pattern: /검은?\s?별/g, result: "검은별" },
+  { pattern: /파왕/g, result: "파리의왕" },
+  { pattern: /게돗물/g, result: "게와수돗" },
+  {
+    pattern: /문방사우/g,
+    result: "시퍼런봄 빨간피터 매미는비 물가의라",
+  },
+  { pattern: /매빨시멸/g, result: "매미는 빨간피터 시퍼런봄 멸종" },
+  { pattern: /피는\s밤/g, result: "피는밤" },
+  { pattern: /난자말/g, result: "난자꾸말" },
+  { pattern: /춤별|춤추는 별/g, result: "춤추는별" },
+  {
+    pattern:
+      /(라이온|열대|상기후|퍼런봄|려운달|한낮|6|서울병|서울|계몽|뭍|마술|은하|넓은밤|하루|에서도|류의맛|검은별|동물)\s콘/g,
+    result: "$1콘",
+  },
+];
+
 const data: Video[] = [
   {
     title: "불구경 2022",
