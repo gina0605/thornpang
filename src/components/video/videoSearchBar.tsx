79a56c861333c47954a315926766f7f1c39f9c05
gina0605 <jina0605@gmail.com>
2024-02-22 02:54:26 +0900

    Update VideoSearchBar css

--- src/components/video/videoSearchBar.tsx
+++ src/components/video/videoSearchBar.tsx
@@ -11,7 +11,7 @@ export const VideoSearchBar = ({ target, search }: VideoSearchBarProps) => (
   <div className="w-full max-w-5xl md:px-1.5">
     <div className="w-full flex justify-end bg-slate-100 md:py-1">
       <div className="w-0 md:w-40 hidden md:flex items-center justify-center space-x-2">
-        <p>검색 대상</p>
+        <p className="text-slate-600 pl-1.5">검색 대상</p>
         <div className="bg-white px-1.5 rounded-sm border-slate-500 border flex">
           <TargetSelect target={target} search={search} />
           <Image
