2d553b0ab3fef709a751bc932f60a8cb194254d8
gina0605 <jina0605@gmail.com>
2024-02-24 05:37:54 +0900

    show AppleProfile when onLoad

--- src/components/profile/appleProfile.tsx
+++ src/components/profile/appleProfile.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const AppleProfile = () => {
   const [visible, setVisible] = useState(false);
-
-  useEffect(() => setVisible(true), []);
 
   return (
     <div className="w-full pt-[75%] relative">
@@ -19,6 +17,7 @@ export const AppleProfile = () => {
         className={`transition-appear duration-1000 ease-out ${
           visible ? "opacity-100" : "md:opacity-0 md:translate-y-4"
         }`}
+        onLoad={() => setVisible(true)}
       />
     </div>
   );
