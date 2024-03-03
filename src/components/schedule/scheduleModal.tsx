83c4f80652c11c1cc77e6ab8b3c8c841c5d78ecc
gina0605 <jina0605@gmail.com>
2024-03-03 21:31:16 +0900

    Change Modal scroll behavior

--- src/components/schedule/scheduleModal.tsx
+++ src/components/schedule/scheduleModal.tsx
@@ -18,35 +18,37 @@ export const ScheduleModal = ({ schedule, closeLink }: ScheduleModalProps) => {
 
   return (
     <ServerModal closeLink={closeLink}>
-      <div className="md:hidden w-[80vw] h-[80vw] max-w-[50vh] max-h-[50vh] relative shrink-0 rounded-t overflow-hidden z-40 bg-black/70">
-        <Image
-          src={`/schedule/${imageSquare ?? image}`}
-          alt="poster"
-          fill
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPU2wQAARQA4liArccAAAAASUVORK5CYII="
-          className={imageSquare ? "object-fill" : "object-contain"}
-          sizes="80vw"
-        />
-      </div>
-      <div className="hidden md:block w-[50vw] h-[70vw] max-w-[57vh] max-h-[80vh] relative shrink-0 rounded-l overflow-hidden z-40">
-        <Image
-          src={`/schedule/${image}`}
-          alt="poster"
-          fill
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPU2wQAARQA4liArccAAAAASUVORK5CYII="
-          className="object-fill"
-          sizes="57vh"
-        />
-      </div>
-      <div className="w-[80vw] max-w-[50vh] md:w-80 md:max-w-80 md:h-[70vw] md:max-h-[80vh] bg-white font-pretendard py-2 px-3 md:px-4 md:py-4 shrink rounded-b md:rounded-l-none md:rounded-r z-40">
-        <ModalBody
-          title={title}
-          date={dateText}
-          content={content}
-          links={links}
-        />
+      <div className="flex flex-col md:flex-row max-h-[85vh] overflow-hidden">
+        <div className="md:hidden w-[80vw] h-[80vw] max-w-[50vh] max-h-[50vh] relative shrink-0 rounded-t overflow-hidden z-40 bg-black/70">
+          <Image
+            src={`/schedule/${imageSquare ?? image}`}
+            alt="poster"
+            fill
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPU2wQAARQA4liArccAAAAASUVORK5CYII="
+            className={imageSquare ? "object-fill" : "object-contain"}
+            sizes="80vw"
+          />
+        </div>
+        <div className="hidden md:block w-[50vw] h-[70vw] max-w-[57vh] max-h-[80vh] relative shrink-0 rounded-l overflow-hidden z-40">
+          <Image
+            src={`/schedule/${image}`}
+            alt="poster"
+            fill
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPU2wQAARQA4liArccAAAAASUVORK5CYII="
+            className="object-fill"
+            sizes="57vh"
+          />
+        </div>
+        <div className="w-[80vw] max-w-[50vh] md:w-80 md:max-w-80 md:h-[70vw] md:max-h-[80vh] bg-white font-pretendard py-2 px-3 md:px-4 md:py-4 shrink rounded-b md:rounded-l-none md:rounded-r z-40 overflow-y-auto">
+          <ModalBody
+            title={title}
+            date={dateText}
+            content={content}
+            links={links}
+          />
+        </div>
       </div>
     </ServerModal>
   );
--- src/components/video/videoModal.tsx
+++ src/components/video/videoModal.tsx
@@ -10,16 +10,13 @@ export interface VideoModalProps {
 export const VideoModal = ({ video }: VideoModalProps) => {
   const { title, thumbnail, subtitle, setlist, info, links, date } = video;
   const content = [
-    {
-      subtitle: "곡명",
-      text: setlist.join(", "),
-    },
+    { subtitle: "곡명", text: setlist.join(", ") },
     { subtitle: "정보", text: info.join(", ") },
   ];
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <DivUnclick className="w-[75vw] h-[42vw] max-w-[89vh] max-h-[50vh] relative shrink-0 rounded-t overflow-hidden z-40">
+    <div className="w-full h-full max-h-[85vh] overflow-hidden flex flex-col items-center justify-center">
+      <DivUnclick className="w-[80vw] h-[45vw] max-w-[89vh] max-h-[50vh] relative shrink-0 rounded-t overflow-hidden z-40">
         <Image
           src={`/video/${thumbnail}`}
           alt="thumbnail"
@@ -30,7 +27,7 @@ export const VideoModal = ({ video }: VideoModalProps) => {
           sizes="(max-width: 768px) 75vw, 89vh"
         />
       </DivUnclick>
-      <DivUnclick className="w-[75vw] max-w-[89vh] bg-white font-pretendard py-2 px-3 shrink rounded-b z-40">
+      <DivUnclick className="w-[80vw] max-w-[89vh] bg-white font-pretendard py-2 px-3 shrink rounded-b z-40 overflow-y-auto">
         <ModalBody
           title={`${title} - ${subtitle}`}
           date={date}
