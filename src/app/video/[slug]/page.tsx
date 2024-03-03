f5b09e4618821c729d5101462b26b741bf506788
gina0605 <jina0605@gmail.com>
2024-03-03 15:20:46 +0900

    Add metadata for video detail page

--- src/app/video/[slug]/page.tsx
+++ src/app/video/[slug]/page.tsx
@@ -1,13 +1,31 @@
 import { Video } from "@/types";
+import { createMetadata } from "@/common/seo";
 import { ServerModal } from "@/components/common/serverModal";
 import { VideoSearchBar } from "@/components/video/videoSearchBar";
 import { Row } from "@/components/video/row";
 import { VideoModal } from "@/components/video/videoModal";
 import data from "@/data/video";
 
+export const generateStaticParams = () => data.map(({ slug }) => ({ slug }));
+export const dynamicParams = false;
+
 interface PageParams {
   slug: string;
 }
+
+export const generateMetadata = ({
+  params: { slug },
+}: {
+  params: PageParams;
+}) => {
+  const { title, subtitle, setlist, info } = data.find(
+    (v) => v.slug === slug
+  ) as Video;
+  return createMetadata(
+    `${title} - ${subtitle}`,
+    `셋리스트: ${setlist.join(", ")} · 정보: ${info.join(", ")}`
+  );
+};
 
 export default ({ params: { slug } }: { params: PageParams }) => {
   const video = data.find((v) => v.slug === slug) as Video;
