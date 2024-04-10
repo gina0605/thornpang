85efa6a0742248101e3bbff8da18ad672c6a29c0
gina0605 <jina0605@gmail.com>
2024-04-10 23:27:27 +0900

    Revalidate schedule cache

--- src/app/schedule/page.ts
+++ src/app/schedule/page.ts
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
+export const revalidate = 15;
+
 export default () => {
   const d = new Date(Date.now() + 32400000);
   redirect(`/schedule/${d.getUTCFullYear()}/${d.getUTCMonth() + 1}`);
