83a43c926f2709229288fb30b68d7080ac516018
gina0605 <jina0605@gmail.com>
2024-12-23 01:03:19 +0900

    Change 쏜탄절 image

--- src/components/schedule/calendar.tsx
+++ src/components/schedule/calendar.tsx
@@ -20,7 +20,7 @@ const CalendarCellIner = ({ red, day, holiday }: CalendarCellInnerProps) => (
       <div className="absolute top-1 bottom-1 -left-px -right-px bg-white bg-opacity-50 rounded -z-10" />
     </div>
     <div className="w-fit h-fit relative" key="info">
-      <p className="relative text-center text-rose-700 text-xs -mt-1 break-normal">
+      <p className="relative text-center text-rose-700 text-xs -mt-1 px-0.5 break-normal">
         {holiday}
       </p>
       <div className="absolute inset-0 -top-1 bg-white bg-opacity-50 rounded -z-10" />
