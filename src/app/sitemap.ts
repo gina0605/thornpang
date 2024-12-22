b45ef8b19bfb7fe75a4da1542629fcb4754a2b44
gina0605 <jina0605@gmail.com>
2024-12-23 01:21:39 +0900

    Fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,16 +12,12 @@ export default () => [
   makeRoute("/lyrics"),
   ...lyricsData.map(({ slug }) => makeRoute(`/lyrics/${slug}`)),
   ...flatten(
-    flatten(
-      range(maxYear - minYear + 1, minYear).map((y) =>
-        range(12, 1).map((m) => [
-          makeRoute(`/schedule/${y}/${m}`),
-          ...Object.entries((scheduleData[y] ?? {})[m] ?? {}).map(
-            ([date, schedule]) => makeRoute(`/schedule/${y}/${m}/${date}`)
-          ),
-        ])
-      )
+    range(maxYear - minYear + 1, minYear).map((y) =>
+      range(12, 1).map((m) => makeRoute(`/schedule/${y}/${m}`))
     )
+  ),
+  ...scheduleData.map(({ year, month, date, slug }) =>
+    makeRoute(`/schedule/${year}/${month}/${slug ?? date}`)
   ),
   makeRoute("/video"),
   ...videoData.map(({ slug }) => makeRoute(`/video/${slug}`)),
