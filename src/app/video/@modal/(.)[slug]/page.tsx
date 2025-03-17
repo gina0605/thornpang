a46b43ddf12e6ff60c13451c91293a6f10691e1f
gina0605 <jina0605@gmail.com>
2025-03-17 22:04:44 +0900

    Add metadata for video detail (client side)

--- src/app/video/@modal/(.)[slug]/page.tsx
+++ src/app/video/@modal/(.)[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { Video } from "@/types";
+import { createMetadata } from "@/common/seo";
 import { VideoModal } from "@/components/video/videoModal";
 import { ClientModal } from "@/components/common/clientModal";
 import data from "@/data/video";
@@ -6,6 +7,20 @@ import data from "@/data/video";
 interface PageParams {
   slug: string;
 }
+
+export const generateMetadata = ({
+  params: { slug },
+}: {
+  params: PageParams;
+}) => {
+  const { title, subtitle, setlist, info } = data.find(
+    (v) => v.slug === slug
+  ) as Video;
+  return createMetadata(
+    `${title} - ${subtitle}`,
+    `셋리스트: ${setlist.join(", ")} · 정보: ${info.join(", ")}`
+  );
+};
 
 export default ({ params: { slug } }: { params: PageParams }) => {
   const video = data.find((v) => v.slug === slug) as Video;
