4763b17f78dd56e940ed375417cfd3a7de89ca53
Yujin Im <69783927+gina0605@users.noreply.github.com>
2025-05-22 00:47:59 +0900

    Update schedule.ts

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -3482,6 +3482,7 @@ const data_2025: Schedule[] = [
       I.FNotice("https://www.instagram.com/p/DJjRKFWyaaI"),
       I.Photo("https://www.instagram.com/p/DJ6xYB-yRQ-"),
       I.Vid("https://www.instagram.com/reel/DJ6ycXzSkF0"),
+      I.Vid("https://www.instagram.com/reel/DJ68EkRS_4V", S.reason),
     ],
   },
   {
